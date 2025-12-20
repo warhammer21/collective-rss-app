0454969b8b0c0cc411506241f0e3cbe28d419d14
warhammer21 <shreyakvashisht21@gmail.com>
2025-12-20 19:08:41 +0530

    endpoint worker initial

--- components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
+++ components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
@@ -6,6 +6,11 @@ import io.collective.workflow.Worker;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.collective.rss.RSS;
+import io.collective.rss.Item;
+import java.util.List;
+
 
 import java.io.IOException;
 
@@ -30,7 +35,17 @@ public class EndpointWorker implements Worker<EndpointTask> {
         String response = template.get(task.getEndpoint(), task.getAccept());
         gateway.clear();
 
+
         { // todo - map rss results to an article infos collection and save articles infos to the article gateway
+            RSS rss = new XmlMapper().readValue(response,RSS.class);
+            List <Item> items = rss.getChannel().getItem();
+            for (int i = 0; i < items.size(); i++){
+                Item item = items.get(i);
+                gateway.save(item.getTitle());
+
+            }
+
+
 
         }
     }
